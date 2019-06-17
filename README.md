c43395ba0e03a993f283b6d5f105aa086cd97f8c
clavierbulb-green <clavierbulb-green@protonmail.com>
2019-06-17 15:35:00 -0700

    Add rudimentary GUI

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ minor scale. Can be useful for encouraging practice evenly throughout all keys.
 
 usage: `./return-scale` or `python3 return-scale.py`
 
-Press <Enter> to print a new scale. Press <Control-C> to quit.
-
+Press the 'New Scale' button or the spacebar to generate the name of a new scale.
+Close the window or press `q` to quit.
--- return-scale.py
+++ return-scale.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import random;
+import PySimpleGUI as sg
+
 keys = ('C', 'C#-Db', 'D', 'D#-Eb', 'E', 'F', 'F#-Gb', 'G', 'G#-Ab', 'A',
         'A#-Bb', 'B')
 modes = ('major', 'minor')
@@ -7,17 +10,20 @@ modes = ('major', 'minor')
 NUM_KEYS = len(keys)
 NUM_MODES = len(modes)
 
-import random;
+layout = [[sg.Text('Your random scale: '), 
+    sg.Text('', key='_OUTPUT_', size=(10,1))],
+          [sg.Button('New Scale')]]
 
-print("A program that returns a random major or minor scale.\n" + 
-      "Press <Enter> to print a new scale. Press <Control-C> to quit.")
+window = sg.Window('Random Scale Generator', layout, return_keyboard_events=True)
 
 while True:
-    try:
-        input()
-    except KeyboardInterrupt:
-        print('\n')
+    event, values = window.Read()
+    if event is None or event == 'q':
         break
-    print(keys[random.randint(0, NUM_KEYS - 1)], 
-      modes[random.randint(0, NUM_MODES - 1)]);
+    
+    if event == 'New Scale':
+        scale = keys[random.randint(0, NUM_KEYS - 1)] + ' ' + modes[
+                random.randint(0, NUM_MODES - 1)]
+        window.Element('_OUTPUT_').Update(scale)
 
+window.Close()
