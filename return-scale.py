5a91dc4820791b573a413c7dbf4d610697d0f39a
clavierbulb-green <clavierbulb-green@protonmail.com>
2019-06-23 10:19:13 -0700

    Edit style to be PEP8 compliant

--- return-scale.py
+++ return-scale.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import random;
+import random
 import PySimpleGUI as sg
 
 keys = ('C', 'C#-Db', 'D', 'D#-Eb', 'E', 'F', 'F#-Gb', 'G', 'G#-Ab', 'A',
@@ -10,19 +10,20 @@ modes = ('major', 'minor')
 NUM_KEYS = len(keys)
 NUM_MODES = len(modes)
 
-layout = [[sg.Image(filename="circle_of_fifths.png")], 
-          [sg.Text('Your random scale: ', size=(20, 2), 
-          font=("Helvetica, 14")), 
-           sg.Text('', key='_OUTPUT_', size=(15,2), font=("Helvetica, 14"))],
+layout = [[sg.Image(filename="circle_of_fifths.png")],
+          [sg.Text('Your random scale: ', size=(20, 2),
+           font=("Helvetica, 14")),
+           sg.Text('', key='_OUTPUT_', size=(15, 2), font=("Helvetica, 14"))],
           [sg.Button('New Scale')]]
 
-window = sg.Window('Random Scale Generator', layout, return_keyboard_events=True)
+window = sg.Window('Random Scale Generator',
+                   layout, return_keyboard_events=True)
 
 while True:
     event, values = window.Read()
     if event is None or event == 'q':
         break
-    
+
     if event == 'New Scale':
         scale = keys[random.randint(0, NUM_KEYS - 1)] + ' ' + modes[
                 random.randint(0, NUM_MODES - 1)]
