26af57d5e2187d3222bbf6990cb51d10bec9ba2a
clavierbulb-green <clavierbulb-green@protonmail.com>
2019-08-21 12:33:45 -0700

    Print the pitches of a returned scale when '--verbose' flag is specified

--- return_scale.py
+++ return_scale.py
@@ -1,18 +1,33 @@
+import music21
 import random
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-n", "--number", type=int, default=1,
                     help="number of scales to return")
+parser.add_argument("-v", "--verbose", action="store_true",
+                    help="increase output verbosity (i.e. print scale pitches)")
 args = parser.parse_args()
 
 
-# Prints the name of a random major or minor scale to std-out
-
-KEYS = ('C', 'C#-Db', 'D', 'D#-Eb', 'E', 'F', 'F#-Gb', 'G', 'G#-Ab', 'A',
-        'A#-Bb', 'B')
+KEYS = ('C', 'D-', 'D', 'E-', 'E', 'F', 'G-', 'G', 'A-', 'A',
+        'B-', 'B')
 MODES = ('major', 'minor')
 
+
+# Print the name of a random major or minor scale to std-out
+
 for n in range(args.number):
-    random_scale = random.choice(KEYS) + ' ' + random.choice(MODES)
-    print(random_scale)
+    tonic = random.choice(KEYS)
+    mode = random.choice(MODES)
+    if mode == 'major':
+        random_scale = music21.scale.MajorScale(tonic)
+    else:
+        random_scale = music21.scale.HarmonicMinorScale(tonic)
+
+    print(random_scale.name.replace('-', 'b'))
+
+    if args.verbose:
+        # Print the pitches of the chosen scale
+        print(' '.join([p.name for p in random_scale.pitches]).replace('-', 'b'))
+        print('\n')
