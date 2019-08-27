755ee084acff0d992282442a37f845a86d7d5798
clavierbulb-green <clavierbulb-green@protonmail.com>
2019-08-27 12:17:33 -0700

    Add optional arguments to set number of accidentals and mode
    The generated keys have less than or equal to the maximum number of
    accidentals. By default this value is 7; it can be set using the -a
    flag.
    
    The mode (major or minor) of the generated keys can be set using the -M
    (major) and the 'm (minor) flags. If not set, the mode of each key is
    random.

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 A small Python program that prints the name of one randomly selected major or 
 minor scale. Can be useful for encouraging practice evenly throughout all keys.
 
-usage: python3 return_scale.py [-h] [-n NUMBER] [-v]
+usage: python3 return_scale.py [-h] [-n NUMBER] [-a ACCIDENTALS] [-M] [-m] [-v]
 
 optional arguments:
   -h, --help            show this help message and exit
+
   -n NUMBER, --number NUMBER
-                        number of scale names to print
+                        number of scales to return
+
+  -a ACCIDENTALS, --accidentals ACCIDENTALS
+                        maximum number of accidentals
+
+  -M, --major           return only major keys
+
+  -m, --minor           return only minor keys
+
   -v, --verbose         increase output verbosity (i.e. print scale pitches)
--- return_scale.py
+++ return_scale.py
@@ -6,16 +6,23 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-n", "--number", type=int, default=1,
                     help="number of scales to return")
+parser.add_argument("-a", "--accidentals", type=int, default=7,
+                    help="maximum number of accidentals")
+parser.add_argument("-M", "--major", action="store_true",
+                    help="return only major keys")
+parser.add_argument("-m", "--minor", action="store_true",
+                    help="return only minor keys")
 parser.add_argument("-v", "--verbose", action="store_true",
                     help="increase output verbosity (i.e. print scale pitches)")
 args = parser.parse_args()
 
 
 # Return a random music21.key.Key
-def randomKey():
-    # Limit keys to those with < 7 accidentals
-    sharps = random.randint(-7, 7)
-    mode = random.choice(('major', 'minor'))
+def randomKey(mode=None, num_accidentals=7):
+    if mode is None:
+        mode = random.choice(('major', 'minor'))
+    # By default, limit keys to those with <= 7 accidentals
+    sharps = random.randint(-num_accidentals, num_accidentals)
     key_signature = music21.key.KeySignature(sharps)
     key = key_signature.asKey(mode)
 
@@ -24,8 +31,14 @@ def randomKey():
 
 def main():
     # Print the name of a random major or minor key to std-out
+    mode = None
+    if args.major:
+        mode = 'major'
+    elif args.minor:
+        mode = 'minor'
+
     for n in range(args.number):
-        key = randomKey()
+        key = randomKey(mode, args.accidentals)
         print(key.name.replace('-', 'b'))
 
         if args.verbose:
